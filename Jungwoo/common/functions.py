3d7476780a6bed9e02ffea14979db8b8416c2dea
DESKTOP-UND38IO\Jungwoo Han <gracejwh@gmail.com>
2017-03-29 20:33:12 +0900

    밑바닥부터 시작하는 딥러닝에 나오는 코드

---
+++ Jungwoo/common/functions.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def softmax(a):
+    c = np.max(a)
+    exp_a = np.exp(a-c)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+
+    return y
+
+
+def cross_entropy_error(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+
+    batch_size = y.shape[0]
+    return -np.sum(np.log(y[np.arange(batch_size), t])) / batch_size
