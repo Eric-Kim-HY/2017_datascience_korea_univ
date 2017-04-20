42607518fbe893a5b96043254ff6ad89a8f7cd3c
DESKTOP-UND38IO\Jungwoo Han <gracejwh@gmail.com>
2017-04-20 18:49:19 +0900

    no message

--- word2vec/utils.py
+++ word2vec/utils.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class util :
     def __init__(self):
         pass
@@ -11,3 +13,31 @@ class util :
 
     def neg_sample(self, nsample):
         return
+    
+    ## update weight matrix
+    
+    alpha = 0.5
+    hiddenSize = 32
+    
+    # compute sigmoid nonlinearity
+    def sigmoid(x):
+        output = 1/(1+np.exp(-x))
+        return output
+    
+    # convert output of sigmoid function to its derivative
+    def sig2deriv(output):
+        return output*(1-output)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
