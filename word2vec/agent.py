cafb8801ad59ead7dbb991fa167331841499c720
Cafebonbon <smilekhy88@daum.net>
2017-04-22 11:54:04 +0900

    no message

--- word2vec/agent.py
+++ word2vec/agent.py
@@ -7,10 +7,10 @@ class agent :
         # define hyperparameter
         self.n_sample = n_sample
         self.n_window = n_window
-        self.learning_rate = learning_rate
-        self.vec_dim = vec_dim
+        self.learning_rate = learning_rate  #learning rate
+        self.vec_dim = vec_dim # 사용할 단어 벡터의 차원
         self.n_words = None
-
+        self.train = 10  # 반복학습횟수
 
         self.dataproc = dataprocessing()
 
@@ -23,9 +23,43 @@ class agent :
         texts = self.dataproc.read_corpus()
 
         # 웨이트 1, 2 들고오기
-        self.W_1, self.W_2 = self.dataproc.build_word_matrix(texts, self.vec_dim)
+        self.W_1 = self.dataproc.build_word_matrix(texts, self.vec_dim)
 
         self.n_words = self.W_1.shape[0]
 
         # 랜덤 샘플링할 index 가져오기
         weigh_idx = self.dataproc.negative_sampling(n_unique_words= self.n_words, n_samples = self.n_sample)
+
+
+    # sampleword : output + negative sample words
+    # inputword, sampleword, label vector : np.array, learning_rate : scalar
+    def train(self, input_word, sample_word, label_vector, learning_rate):
+
+
+
+        # forward propagation
+        layer_0 = X  # 0번째 layer == input matrix
+        layer_1 = relu(np.dot(layer_0, W_0))  # 1번째 layer: layer0에 W_0 행렬곱
+        layer_2 = relu(np.dot(later_1, W_1))  # 2번째 layer: layer1에 W_1 행렬곱
+
+        # calculate error
+        layer_2_error = layer_2 - y  # error는 원래값(y)과의 차이
+
+        if (j % 10000) == 0:
+            print(str(j) + "번 학습 후 error:" + str(
+                np.mean(np.abs(layer_2_error))))  # 10000번 학습할때마다 layer2 error 가 얼만큼 변하는지 보기
+
+            #       layer_2_delta = layer_2_error*sig2deriv(layer_2)         # reLU를 사용하면 오른쪽에 곱해지는 값이 항상 1
+
+            #       layer_1_error = layer_2_delta.dot(W_1.T)
+        layer_1_error = layer_2_error.dot(W_1.T)  # reLU의 미분값은 양의 값에서 항상 1 이므로 'delta = error'
+
+        #       layer_1_delta = layer_1_error*sig2deriv(layer_1)
+
+        # update weight matrix
+        W_1 -= alpha * (layer_1.T.dot(layer_2_error))
+        W_0 -= alpha * (layer_0.T.dot(layer_1_error))
+
+        #
+        return updated_sample_word
+
--- word2vec/data_proc.py
+++ word2vec/data_proc.py
@@ -18,16 +18,18 @@ class dataprocessing :
 
     def build_word_matrix(self, tokens, vector_dim):
 
-        n_words = len(tokens)
+
         # build standard word matrix
         word_matrix = pd.DataFrame(index = set(tokens),columns=range(vector_dim), dtype=np.float32)
+        #TODO initialization normal (0,0.5)
 
-        TF = nltk.FreqDist(tokens)
-
-        # weight matrix 2개 관리하기 때문에 한번 연산으로 두 개 반환, 단어 빈도수 반환, 단어 종류수 반환
-        return (word_matrix, word_matrix)
+        # weight matrix 2개 관리하기 때문에 한번 연산으로 두 개 반환
+        return word_matrix
 
     # 총단어의 갯수(idx), sampling할 단어 수 n개 입력해 n개의 idx를 받아온다
     def negative_sampling(self, n_unique_words, n_samples):
         return np.random.randint(low = 0, high = n_unique_words, size = n_samples)
+
+
+    def
 
