2b6997bd296d6a3206cd9432b2bd93e49b2a7682
Cafebonbon <smilekhy88@daum.net>
2017-05-07 12:03:54 +0900

    Treated the dimension error in sklearn's cosine similarity

--- word2vec/word_analyze.py
+++ word2vec/word_analyze.py
@@ -10,9 +10,9 @@ class word_analysis():
     @jit
     def __init__(self):
         self.k = 10          # 상위 몇 개의 단어를 받을지 정수 입력
-        self.W = pd.read_csv('./wordvector.csv', header= 0, index_col = 0) # weight parameter 불러오기
-        self.W.dropna(how='any', inplace=True) # None 포함하는 행 제거
-        self.COSIM = sklearn.metrics.pairwise.cosine_similarity # Cosine similarity
+        self.W = pd.read_csv('./wordvector.csv', header= 0, index_col = 0)  # weight parameter 불러오기
+        self.W.dropna(how='any', inplace=True)                              # None 포함하는 행 제거
+        self.COSIM = sklearn.metrics.pairwise.cosine_similarity             # Cosine similarity function
 
     @jit
     def getVector(self, word):
@@ -20,35 +20,44 @@ class word_analysis():
         if word in self.W.index.tolist():
             return self.W.ix[word].values
         else:
-            print("해당 단어는 우리의 딕셔너리에 없습니다. 다른 단어를 입력해주세요.")
+            print("{0} 단어는 우리의 딕셔너리에 없습니다. 다른 단어를 입력해주세요.".format(word))
 
 
     @jit
     def nearestWord(self, v, k):
-        # 기준 단어의 벡터(v)와 가장 가까운 상위 k개의 단어를 리턴한다.
+        ## 기준 단어의 벡터(v)와 가장 가까운 상위 k개의 단어를 리턴한다. ##
+
+        # cosine similarity 칼럼 생성을 위해 weight를 새로운 matrix로 복사
         W = self.W
-        W['sim'] = self.COSIM(W, v)
 
+        # cosine similarity 칼럼 생성,
+        W['sim'] = self.COSIM(W.as_matrix().reshape([W.shape[0],W.shape[1]]),v)
+
+        # similarity 연산을 위해 vector reshape
+        v = v.reshape([1,W.shape[1]])
+
+        # Similarity 높은 순으로 정렬
         W.sort_values(by='sim', axis=0, ascending=False, inplace=True, kind='quicksort', na_position='last')
+
+        # 상위 k 개의 단어 list 형태로 저장
         ret = W.index[:k].tolist()
+
         return ret
-
-
 
 
     # <1> 유사한 관계의 단어 추출
     # word analogy  vector('woman') + [ vector('king') - vector('man') ] +   => vector('queen')
     @jit
     def analogy(self):
-        #TODO C 를 기준으로 산출되는 벡터 계산 문제점 확인
+
         print("A : B의 관계는 C : D의 관계와 같다")
         vec_A = self.getVector(input("A의 자리에 넣을 단어를 입력하세요 : "))
         vec_B = self.getVector(input("B의 자리에 넣을 단어를 입력하세요 : "))
         vec_C = self.getVector(input("C의 자리에 넣을 단어를 입력하세요 : "))
-        vec_D = vec_C + vec_B - vec_A
+        vec_D = vec_B - vec_A + vec_C
         words_nearD = self.nearestWord(vec_D, self.k)
-        result = words_nearD
-        print("D의 자리에 가장 가까운 {0}개의 단어들: {1}".format( self.k, result))
+
+        print("D의 자리에 가장 가까운 {0}개의 단어들: {1}".format( self.k, words_nearD))
 
 
     # <2> 가장 유사한 N개의 단어 추출
