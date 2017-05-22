914f290827c42ad126a3d7bb45759395fd77ff1b
Cafebonbon <smilekhy88@daum.net>
2017-05-23 01:05:34 +0900

    word preprocessing, data transform for doc2vec, etx

--- trip2vec/trip2vec_model/simple_model.py
+++ trip2vec/trip2vec_model/simple_model.py
@@ -1,11 +1,7 @@
 import gensim
 import pandas as pd
-
-
-
-
-
-
+from sklearn import utils
+import re
 ### Set Hyperparameter ###
 VECTOR_SIZE = 300
 WINDOW = 10
@@ -14,24 +10,62 @@ PARALELL_SIZE = 12
 LEARNING_RATE = 0.025
 MIN_LEARNING_RATE = 0.025
 
+ITERATIONS = 10
 
+### Set Functions ###
+
+# Preprocessing function
+def cleanText(corpus) :
+    punctuation = ".,?!:;(){}[]"
+    for char in punctuation: corpus = corpus.replace(char,"")
+    corpus = corpus.lower()
+    corpus = corpus.split()
+    return corpus
+
+# Attraction preprocessing
+def treat_attraction(s):
+    if s.startswith('about ') :
+        s = s[6:]
+    elif s.startswith('A about '):
+        s = s[8:]
+    return s
+
+# label the dataframe and get unique words
+def label_dataframe(df):
+    LabeledSentence = gensim.models.doc2vec.LabeledSentence
+    ret1 = []
+    ret2 = []
+    for idx, row in df.iterrows():
+        temp_corpus = cleanText(row['review_text'])
+        trip_id = row['attraction']
+        ret1.append(LabeledSentence(temp_corpus, trip_id))
+        ret2 = list(set(ret2 + temp_corpus))
+
+    return (ret1, ret2)
+
+
+### Proceed the doc2vec
 
 # Load csv data
 raw_data = pd.read_csv('./trip.csv', header = 0 )
 
-# Preprocess the data
+# Paris data 만 가져오기
+raw_data = raw_data[raw_data['city'] =='Paris']
 
-#TODO erase about some attraction name
+# Attraction 앞 about 제거
+raw_data['attraction'] = raw_data['attraction'].apply(treat_attraction)
 
+# corpus building, prepare data for doc2vec
+train_data, all_corpus = label_dataframe(raw_data)
 
-
-
-# Set data to fit Gensim doc2vec
-LabeledSentence = gensim.models.doc2vec.LabeledSentence
-gensim.models.doc2ec.TaggedDocumnet()
 
 # Define model
 model = gensim.models.Doc2Vec(size = VECTOR_SIZE, window = WINDOW, min_count= MIN_COUNT,
                               workers=PARALELL_SIZE, alpha=LEARNING_RATE, min_alpha=MIN_LEARNING_RATE)
 
-model.build_vocab(data)
+model.build_vocab(all_corpus)
+
+for epoch in range(ITERATIONS):
+    model.train(utils.shuffle(train_data))
+
+model.save('Model_after_train')
